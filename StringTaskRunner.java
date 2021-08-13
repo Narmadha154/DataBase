6dba47da9df5c95383c930c435656c25e4e56752
Narmadha154 <85552355+Narmadha154@users.noreply.github.com>
2021-08-13 16:44:58 +0530

    Add files via upload

--- StringTaskRunner.java
+++ StringTaskRunner.java
@@ -1,70 +1,96 @@
-package task1;
+
 import java.util.Scanner;
 public class StringTaskRunner{
+public static String getString(){
+System.out.println("Enter the string:");
+Scanner sc1=new Scanner(System.in);
+String str=sc1.next();
+System.out.println("the string is:"+ str);
+return str;
+}
+public static String getMultipleString(){
+System.out.println("Enter the string:");
+Scanner sc1=new Scanner(System.in);
+String str=sc1.nextLine();
+System.out.println("the string is:"+ str);
+return str;
+}
 public static void main(String [] args){
 System.out.println("Enter the program number:");
 Scanner sc=new Scanner(System.in);
 int number=sc.nextInt();
-System.out.println("Enter the string:");
-String str=sc.nextLine();
-System.out.println("the string is:"+ str);
  switch(number){
      case 1:
-        int length=task.findLength(str);
+        String str=getString();
+        int length=StringTask.findLength(str);
         System.out.println("length of the string:"+length);
         break;
      case 2:
-        task.convertIntoArray(str);
+        String str1=getString();
+        StringTask.convertIntoArray(str1);
         break;
     case 3:
-        char character=task.findLastOne(str);
+        String str2=getString();
+        char character=StringTask.findLastOne(str2);
         System.out.println("Last character in the string is:"+character);
         break;
     case 4:
-        int count=task.findOccurence(str);
+        String str3=getString();
+        int count=StringTask.findOccurence(str3);
         System.out.println(count);
         break;
     case 5:
-        int position=task.findGreatestPosition(str);
+        String str4=getString();
+        int position=StringTask.findGreatestPosition(str4);
         System.out.println("Greatest position of the letter is:"+position);
         break;
     case 6:
-       String str=task.printLastFive(str);
-       System.out.println("Required String is:"+str1);
-       break;
-    case 7:
-       String string=task.printFirstThree(str);
+       String str5=getString();
+       String string=StringTask.printLastFive(str5);
        System.out.println("Required String is:"+string);
        break;
+    case 7:
+       String str6=getString();
+       String string1=StringTask.printFirstThree(str6);
+       System.out.println("Required String is:"+string1);
+       break;
     case 8:
-       String replacedString=task.replaceThree(str);
+       String str7=getString();
+       String replacedString=StringTask.replaceThree(str7);
        System.out.println("Replaced String is:"+replacedString);
        break;
    case 9:
-       boolean value=task.checkEnd(str);
+       String str8=getString();
+       boolean value=StringTask.checkEnd(str8);
        System.out.println("String ends with le:"+value);
        break;
    case 10:
-       boolean value2=task.checkStart(str);
+       String str9=getString();
+       boolean value2=StringTask.checkStart(str9);
        System.out.println("String starts with ent:"+ value2);
        break;    
    case 11:
-       String lowercase=task.convertLowerCase(str);
+       String str10=getString();
+       String lowercase=StringTask.convertLowerCase(str10);
        System.out.println("Required String is:"+lowercase);
        break;
    case 12:
-       String uppercase=task.convertUpperCase(str);
+       String str11=getString();
+       String uppercase=StringTask.convertUpperCase(str11);
        System.out.println("Required String is:"+uppercase);
        break;              
    case 13:
-       String reversedString=task.reverseString(str);
+       String str12=getString();
+       String reversedString=StringTask.reverseString(str12);
        System.out.println("Reversed String is :" +reversedString);
        break;
    case 14:
-       System.out.println("Multiple Strings:"+str);
+       String str13=getMultipleString();
+       System.out.println("Multiple Strings:"+str13);
        break;
    case 15:
-       String concatenatedString=task.concatenateStrings(str);
+       String str14=getMultipleString();
+       String concatenatedString=StringTask.concatenateStrings(str14);
        System.out.println("String without space is:"+concatenatedString);
        break;
   default:
